dd583d5a3c89e2306e7058267dd59d591364723e
Scott W Harden <swharden@gmail.com>
2022-06-20 21:41:20 -0400

    create log anonymizer

---
+++ dev/anonymize/Program.cs
@@ -0,0 +1,23 @@
+﻿Anonymize("../../htdocs/logs/2022-06-21.txt");
+
+static string AnonymizeIp(string ip)
+{
+    string[] parts = ip.Split(".");
+    return $"{parts[0]}.{parts[1]}.x.x";
+}
+
+static void Anonymize(string filePath)
+{
+    System.Text.StringBuilder sb = new();
+
+    string[] lines = File.ReadAllLines(filePath);
+    foreach (string line in lines)
+    {
+        string[] parts = line.Split(" ", 5);
+        parts[1] = AnonymizeIp(parts[1]);
+        string line2 = string.Join(" ", parts);
+        sb.AppendLine(line2);
+    }
+
+    File.WriteAllText(filePath, sb.ToString());
+}
